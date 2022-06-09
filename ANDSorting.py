dc21d1692f3a48e9dc7ff1843fd3c8129882cdf7
Stephvideo <willstephens2000@gmail.com>
2022-06-09 15:43:21 -0500

    6/9/2022

---
+++ ANDSorting.py
@@ -0,0 +1,34 @@
+# https://codeforces.com/problemset/problem/1682/B
+
+for t in range(int(input())):
+    n = int(input())
+
+    arr = list(map(int, input().split()))
+
+    sortedArr = sorted(arr)
+    #print(sortedArr)
+    ans = 0
+    x = sortedArr[-1]
+    high = sortedArr[-1]
+    low = 0
+    while (high >= low):
+        correct = True
+        x = high + low // 2
+        for i in range(n):
+            if arr[i] == sortedArr[i]: continue
+
+            if arr[i] & sortedArr[i] != x:
+                
+                correct = False
+                break 
+            # else:
+            #     for j in range(i+1, n):
+            #         if arr[j] == sortedArr[i]:
+            #             arr[i], arr[j] == arr[j], arr[i] 
+        if correct:
+            print(x)
+            break        
+        x -= 1
+
+    if x == 0:
+        print(0)
